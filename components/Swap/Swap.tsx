c6cb986843c01e349462283791d4d6c1e37e0ecc
haracena <haracena.dev@gmail.com>
2025-11-13 23:31:13 -0300

    Adds change token ui

--- components/Swap/Swap.tsx
+++ components/Swap/Swap.tsx
@@ -1,11 +1,26 @@
 import { Token } from '@/interfaces/Token'
 import { mockTokens } from '@/mock/tokens'
 import { ArrowDownUp } from 'lucide-react'
+import { useState } from 'react'
 
 interface SwapProps {
   selectedToken: Token
 }
 export default function Swap({ selectedToken }: SwapProps) {
+  const [sellToken, setSellToken] = useState(mockTokens[1])
+  const [buyToken, setBuyToken] = useState(selectedToken)
+  const [rotateChangeButton, setRotateChangeButton] = useState(0)
+
+  const handleChangeButtonClick = () => {
+    setRotateChangeButton((prev) => {
+      if (prev === 0) {
+        return 180
+      }
+      return 0
+    })
+    setSellToken((prev) => (prev === sellToken ? buyToken : sellToken))
+    setBuyToken((prev) => (prev === buyToken ? sellToken : buyToken))
+  }
   return (
     <div className="grid grid-cols-1 gap-2">
       <div className="relative rounded-lg border border-white/20 bg-white/10 p-2 shadow-md backdrop-blur-md">
@@ -13,13 +28,11 @@ export default function Swap({ selectedToken }: SwapProps) {
         <div className="flex items-center justify-between gap-2">
           <div className="flex items-center gap-2">
             <img
-              src={mockTokens[1].iconUrl}
+              src={sellToken.iconUrl}
               alt="logo"
               className="size-8 rounded-full"
             />
-            <span className="font-regular text-2xl">
-              {mockTokens[1].symbol}
-            </span>
+            <span className="font-regular text-2xl">{sellToken.symbol}</span>
           </div>
           <input
             className="w-full text-end text-3xl font-medium focus:outline-none"
@@ -40,21 +53,24 @@ export default function Swap({ selectedToken }: SwapProps) {
           </p>
         </div>
       </div>
-      <div className="botom-0 relative left-1/2 z-20 -mt-4 w-fit -translate-x-1/2 rounded-lg border border-white/20 bg-neutral-700/90 p-2 shadow-md backdrop-blur-md">
-        <ArrowDownUp className="size-4 text-white" />
+      <div
+        onClick={handleChangeButtonClick}
+        className="botom-0 relative left-1/2 z-20 -mt-4 w-fit -translate-x-1/2 cursor-pointer rounded-lg border border-white/20 bg-neutral-700/90 p-2 shadow-md backdrop-blur-md"
+      >
+        <ArrowDownUp
+          className={`size-4 text-white transition-transform duration-300 ease-in-out ${rotateChangeButton === 180 ? 'rotate-180' : 'rotate-0'}`}
+        />
       </div>
       <div className="relative -mt-4 rounded-lg border border-white/20 bg-white/10 p-2 shadow-md backdrop-blur-md">
         <p className="mb-2 text-xs text-neutral-300">You receive</p>
         <div className="flex items-center justify-between gap-2">
           <div className="flex items-center gap-2">
             <img
-              src={selectedToken.iconUrl}
+              src={buyToken.iconUrl}
               alt="logo"
               className="size-8 rounded-full"
             />
-            <span className="font-regular text-2xl">
-              {selectedToken.symbol}
-            </span>
+            <span className="font-regular text-2xl">{buyToken.symbol}</span>
           </div>
           <input
             className="w-full text-end text-3xl font-medium focus:outline-none"
